5f08fc4a08d8ede335b13816d8500f6f724e363f
Charvi Mendiratta <charvi077@gmail.com>
2022-12-04 15:53:44 +0530

    Updated : aoc 2022 - day4
    Signed-off-by: Charvi Mendiratta <charvi077@gmail.com>

---
+++ aoc_soln_2022/day4b.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <vector>
+#include <sstream>
+
+int count = 0;
+
+int main() {
+    std::fstream newfile;
+
+
+    newfile.open("../input/day4a.txt", std::ios::in);
+    if (newfile.is_open()) {
+        std::string num_txt;
+        while (getline(newfile, num_txt)) {
+            std::stringstream ss;
+            std::string temp_string;
+            int number;
+            std::vector<int> num_vec;
+
+            // std::cout << "num txt : " << num_txt << std::endl;
+            int i = 0;
+            while (num_txt[i]) {
+                if (num_txt[i] == ',' || num_txt[i] == '-')
+                    num_txt[i] = ' ';
+                i++;
+            }
+            std::cout << "num txt : " << num_txt << std::endl;
+            ss << num_txt;
+
+            while (!ss.eof()) {
+                ss >> temp_string;
+
+                if (std::stringstream(temp_string) >> number) {
+                    // std::cout << number << " " << std::endl;
+                    num_vec.push_back(number);
+                }
+
+                temp_string = "";
+            }
+
+            if ((num_vec[0] >= num_vec[2] && num_vec[0] <= num_vec[3]) ||
+                (num_vec[1] <= num_vec[3]) && num_vec[1] >= num_vec[2]) {
+                count++;
+                std::cout << "yes" << std::endl;
+            } else if ((num_vec[2] >= num_vec[0] && num_vec[2] <= num_vec[1]) ||
+                       (num_vec[3] <= num_vec[1]) && num_vec[3] >= num_vec[0]) {
+                count++;
+                std::cout << "yes" << std::endl;
+            }
+        }
+    }
+    newfile.close();
+
+    std::cout << "Result is : " << count << std::endl;
+}
